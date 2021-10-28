908d6cca4fe77e7cb4a799bf6386d63884fb9e6f
Rafael Amed Valenzuela <69322700+rafamedtech@users.noreply.github.com>
2021-10-27 20:20:20 -0700

    main header image updated

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+// Dom items
 const backButton = document.querySelector('.btn');
 const menuTitle = document.getElementById('menu');
 const menuHeader = document.getElementById('header');
 const logo = document.querySelector('.logo');
 
+// Event Listeners
 menuTitle.addEventListener('click', showMainMenu);
+window.addEventListener('scroll', showMenu);
 
+// Functions
 function showMainMenu() {
   document.documentElement.requestFullscreen().catch((e) => {
     console.log(e);
@@ -13,10 +17,6 @@ function showMainMenu() {
   menuHeader.classList.remove('active');
   logo.classList.remove('active');
 }
-
-document.addEventListener('dblclick', () => {});
-
-window.addEventListener('scroll', showMenu);
 
 function showMenu() {
   if (window.scrollY > backButton.offsetHeight + 150) {
